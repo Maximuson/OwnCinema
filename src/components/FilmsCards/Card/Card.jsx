a82388221f74e74c00cda4737cd5c46458a83d43
Maximuson <maxpavlenko54@gmail.com>
2019-03-19 16:14:59 +0200

    Рабочая версия на случай если всё пойдет не так

--- src/components/FilmsCards/Card/Card.jsx
+++ src/components/FilmsCards/Card/Card.jsx
@@ -6,24 +6,28 @@ import state from '../../../redux/state';
 
 
 const Card = (props) => {
-    function getPlaces(){
+
+    /*
+    * Функция подставляет все места фильма в глобальный state.places
+    * при изменении глобального places изменяется places конкретного фильма
+    */
+    function bookPlace() {
         state.places = props.film.places;
         console.log(state.places)
     }
     return (
-       
-        <div className={s.wrapper}>     
-            <img  className={s.img} />
-            {/* <NavLink to={`/films/${props.film.name}`}><strong className={s.title}>{props.film.name}</strong> </NavLink> */}
-            <NavLink to={`/chosePlace`}><strong onClick={getPlaces} className={s.title}>{props.film.name}</strong> </NavLink>
-            <div  className="prices">
+
+        <div className={s.wrapper}>
+            <img className={s.img} />
+            <NavLink  className={s.title} to={`/chosePlace`}>
+                <strong onClick={bookPlace}>
+                    {props.film.name}
+                </strong> </NavLink>
+            <div className={s.time}>
                 <p>{props.film.time}</p>
             </div>
-            
-            {/* <Places places = {props.film.places} /> */}
-            
         </div>
-       
+
     )
 }
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,61 +1,6 @@
-// class Places{
-//     places = [
-//         { placeNum: 1, isBrone: false },
-//         { placeNum: 10, isBrone: false },
-//         { placeNum: 19, isBrone: false },
-//         { placeNum: 28, isBrone: false },
-//         { placeNum: 37, isBrone: false },
-//         { placeNum: 46, isBrone: false },
-//         { placeNum: 2, isBrone: false },
-//         { placeNum: 11, isBrone: false },
-//         { placeNum: 20, isBrone: false },
-//         { placeNum: 29, isBrone: false },
-//         { placeNum: 38, isBrone: false },
-//         { placeNum: 47, isBrone: false },
-//         { placeNum: 3, isBrone: false },
-//         { placeNum: 12, isBrone: false },
-//         { placeNum: 21, isBrone: false },
-//         { placeNum: 30, isBrone: false },
-//         { placeNum: 39, isBrone: false },
-//         { placeNum: 48, isBrone: false },
-//         { placeNum: 4, isBrone: false },
-//         { placeNum: 13, isBrone: false },
-//         { placeNum: 22, isBrone: false },
-//         { placeNum: 31, isBrone: false },
-//         { placeNum: 40, isBrone: false },
-//         { placeNum: 49, isBrone: false },
-//         { placeNum: 5, isBrone: false },
-//         { placeNum: 14, isBrone: false },
-//         { placeNum: 23, isBrone: false },
-//         { placeNum: 32, isBrone: false },
-//         { placeNum: 41, isBrone: false },
-//         { placeNum: 50, isBrone: false },
-//         { placeNum: 6, isBrone: false },
-//         { placeNum: 15, isBrone: false },
-//         { placeNum: 24, isBrone: false },
-//         { placeNum: 33, isBrone: false },
-//         { placeNum: 42, isBrone: false },
-//         { placeNum: 51, isBrone: false },
-//         { placeNum: 7, isBrone: false },
-//         { placeNum: 16, isBrone: false },
-//         { placeNum: 25, isBrone: false },
-//         { placeNum: 34, isBrone: false },
-//         { placeNum: 43, isBrone: false },
-//         { placeNum: 52, isBrone: false },
-//         { placeNum: 8, isBrone: false },
-//         { placeNum: 17, isBrone: false },
-//         { placeNum: 26, isBrone: false },
-//         { placeNum: 35, isBrone: false },
-//         { placeNum: 44, isBrone: false },
-//         { placeNum: 53, isBrone: false },
-//         { placeNum: 9, isBrone: false },
-//         { placeNum: 18, isBrone: false },
-//         { placeNum: 27, isBrone: false },
-//         { placeNum: 36, isBrone: false },
-//         { placeNum: 45, isBrone: false },
-//         { placeNum: 54, isBrone: false },
-//     ]
-// }
+let rerenderEntireTree = () => {
+    console.log('state changed')
+  }
 
 let state = {
     places : [
@@ -234,62 +179,12 @@ let state = {
         console.log(state.places)
     }
 
-    // places: [
-    //     { placeNum: 1, isBrone: false },
-    //     { placeNum: 10, isBrone: false },
-    //     { placeNum: 19, isBrone: false },
-    //     { placeNum: 28, isBrone: false },
-    //     { placeNum: 37, isBrone: false },
-    //     { placeNum: 46, isBrone: false },
-    //     { placeNum: 2, isBrone: false },
-    //     { placeNum: 11, isBrone: false },
-    //     { placeNum: 20, isBrone: false },
-    //     { placeNum: 29, isBrone: false },
-    //     { placeNum: 38, isBrone: false },
-    //     { placeNum: 47, isBrone: false },
-    //     { placeNum: 3, isBrone: false },
-    //     { placeNum: 12, isBrone: false },
-    //     { placeNum: 21, isBrone: false },
-    //     { placeNum: 30, isBrone: false },
-    //     { placeNum: 39, isBrone: false },
-    //     { placeNum: 48, isBrone: false },
-    //     { placeNum: 4, isBrone: false },
-    //     { placeNum: 13, isBrone: false },
-    //     { placeNum: 22, isBrone: false },
-    //     { placeNum: 31, isBrone: false },
-    //     { placeNum: 40, isBrone: false },
-    //     { placeNum: 49, isBrone: false },
-    //     { placeNum: 5, isBrone: false },
-    //     { placeNum: 14, isBrone: false },
-    //     { placeNum: 23, isBrone: false },
-    //     { placeNum: 32, isBrone: false },
-    //     { placeNum: 41, isBrone: false },
-    //     { placeNum: 50, isBrone: false },
-    //     { placeNum: 6, isBrone: false },
-    //     { placeNum: 15, isBrone: false },
-    //     { placeNum: 24, isBrone: false },
-    //     { placeNum: 33, isBrone: false },
-    //     { placeNum: 42, isBrone: false },
-    //     { placeNum: 51, isBrone: false },
-    //     { placeNum: 7, isBrone: false },
-    //     { placeNum: 16, isBrone: false },
-    //     { placeNum: 25, isBrone: false },
-    //     { placeNum: 34, isBrone: false },
-    //     { placeNum: 43, isBrone: false },
-    //     { placeNum: 52, isBrone: false },
-    //     { placeNum: 8, isBrone: false },
-    //     { placeNum: 17, isBrone: false },
-    //     { placeNum: 26, isBrone: false },
-    //     { placeNum: 35, isBrone: false },
-    //     { placeNum: 44, isBrone: false },
-    //     { placeNum: 53, isBrone: false },
-    //     { placeNum: 9, isBrone: false },
-    //     { placeNum: 18, isBrone: false },
-    //     { placeNum: 27, isBrone: false },
-    //     { placeNum: 36, isBrone: false },
-    //     { placeNum: 45, isBrone: false },
-    //     { placeNum: 54, isBrone: false },
-    // ]
+
 }
-window.state = state
+window.state = state;
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
+  }
+
 export default state;
